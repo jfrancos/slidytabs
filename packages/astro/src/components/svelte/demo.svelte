d55f581513e2ab7da9b1a2b4dcc80eefd0d2b6c4
Justin Francos <justinfrancos@gmail.com>
2025-11-19 09:29:06 -0500

    highlight attachment

--- packages/astro/src/components/svelte/demo.svelte
+++ packages/astro/src/components/svelte/demo.svelte
@@ -2,13 +2,19 @@
   import { codeToHtml } from "shiki";
   import BasicExample from "./basicExample.svelte";
   import exampleCode from "./basicExample.svelte?raw";
+
+  const highlight = "{@attach () => {}}";
+  const start = exampleCode.indexOf(highlight);
+  const end = start + highlight.length;
+  const properties = { class: "highlighted" };
+  const decoration = { start, end, properties };
 </script>
 
 <div class="flex flex-col items-center p-4">
   Svelte / shadcn-svelte
 
   <BasicExample />
-  {#await codeToHtml( exampleCode, { lang: "svelte", theme: "github-dark" } ) then value}
+  {#await codeToHtml( exampleCode, { lang: "svelte", theme: "github-dark", decorations: [decoration] } ) then value}
     <div class="text-sm *:rounded-lg *:px-6 *:py-5">{@html value}</div>
   {/await}
 </div>
