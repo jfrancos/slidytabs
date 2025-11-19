671663082ded6c111c09030915bd7d3cffb7252d
Justin Francos <justinfrancos@gmail.com>
2025-11-19 09:15:48 -0500

    updates

---
+++ packages/astro/src/components/svelte/basicExample.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import * as Tabs from "./tabs";
+  // import { rangetabs } from "slidytabs";
+</script>
+
+<Tabs.Root value="account" {@attach () => {}}>
+  <Tabs.List>
+    <Tabs.Trigger value="account">Account</Tabs.Trigger>
+    <Tabs.Trigger value="password">Password</Tabs.Trigger>
+  </Tabs.List>
+</Tabs.Root>
