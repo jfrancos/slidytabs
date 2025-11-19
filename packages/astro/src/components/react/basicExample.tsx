81c56070c5e1d86d09ed9bd2b5da9a619f586ee3
Justin Francos <justinfrancos@gmail.com>
2025-11-19 00:08:31 -0500

    formatted code

---
+++ packages/astro/src/components/react/basicExample.tsx
@@ -0,0 +1,11 @@
+import { Tabs, TabsList, TabsTrigger } from "./tabs.js";
+// import { rangetabs } from "slidytabs";
+
+export const BasicExample = () => (
+  <Tabs defaultValue="account" ref={() => {}}>
+    <TabsList>
+      <TabsTrigger value="account">Account</TabsTrigger>
+      <TabsTrigger value="password">Password</TabsTrigger>
+    </TabsList>
+  </Tabs>
+);
--- packages/astro/src/components/react/demo.tsx
+++ packages/astro/src/components/react/demo.tsx
@@ -1,13 +1,27 @@
-import { Tabs, TabsList, TabsTrigger } from "./tabs";
+import { BasicExample } from "./basicExample";
+import exampleCode from "./basicExample?raw";
+import ShikiHighlighter from "react-shiki";
 
-export default () => (
-  <div className="flex items-center flex-col p-4">
-    React / shadcn
-    <Tabs defaultValue="account">
-      <TabsList>
-        <TabsTrigger value="account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
-      </TabsList>
-    </Tabs>
-  </div>
-);
+const highlight = "ref={() => {}}";
+const start = exampleCode.indexOf(highlight);
+const end = start + highlight.length;
+const properties = { class: "bg-neutral-600 rounded-sm p-1 -m-1" };
+
+const decoration = { start, end, properties };
+
+export default () => {
+  return (
+    <div className="flex items-center flex-col p-4">
+      React / shadcn
+      <BasicExample />
+      <ShikiHighlighter
+        className="text-sm"
+        language="jsx"
+        theme="github-dark"
+        decorations={[decoration]}
+      >
+        {exampleCode}
+      </ShikiHighlighter>
+    </div>
+  );
+};
