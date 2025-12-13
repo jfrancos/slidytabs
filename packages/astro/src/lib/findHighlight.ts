ae81033c5c616a7297f230ed333a15ab7c69218a
Justin Francos <justinfrancos@gmail.com>
2025-12-13 09:31:53 -0500

    auto highlight

---
+++ packages/astro/src/lib/findHighlight.ts
@@ -0,0 +1,43 @@
+import { parse as parseTSX } from "@babel/parser";
+import { parse as parseSvelte } from "svelte/compiler";
+import { walk } from "zimmerframe";
+import type { JSX } from "@babel/types";
+
+export const extractRef = (source: string) => {
+  const ast = parseTSX(source, {
+    sourceType: "module",
+    plugins: ["jsx", "typescript"],
+  });
+  let refString;
+  walk(ast.program as unknown as JSX, null, {
+    JSXOpeningElement(node, { next }) {
+      if (node.name.type === "JSXIdentifier" && node.name.name === "Tabs") {
+        next();
+      }
+    },
+    JSXAttribute(node, { stop }) {
+      if (node.name.name === "ref") {
+        refString = source.slice(node.start!, node.end!);
+        stop();
+      }
+    },
+  });
+  return refString;
+};
+
+export function extractAttachment(source: string) {
+  const ast = parseSvelte(source);
+  let attachString;
+  walk(ast.html, null, {
+    InlineComponent(node, { next }) {
+      if (node.name === "Tabs.Root") {
+        next();
+      }
+    },
+    AttachTag(node, { stop }) {
+      attachString = source.slice(node.start, node.end);
+      stop();
+    },
+  });
+  return attachString;
+}
