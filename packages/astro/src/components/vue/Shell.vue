809b9191509cab298f1dc27fa25b3de57bdd0950
Justin Francos <justinfrancos@gmail.com>
2025-12-19 15:26:23 -0500

    demo sans slidytabs

---
+++ packages/astro/src/components/vue/Shell.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from "vue";
+
+const props = defineProps<{ filename: string }>();
+const Component = defineAsyncComponent(
+  () => import(`./examples/${props.filename}.vue`)
+);
+</script>
+
+<template>
+  <Component />
+</template>
---
+++ packages/astro/src/components/vue/examples/Tabs.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../shadcn/tabs";
+</script>
+
+<template>
+  <Tabs default-value="account" class="text-center">
+    <TabsList>
+      <TabsTrigger value="account"> Account </TabsTrigger>
+      <TabsTrigger value="password"> Password </TabsTrigger>
+    </TabsList>
+    <TabsContent value="account">Account</TabsContent>
+    <TabsContent value="password">Password</TabsContent>
+  </Tabs>
+</template>
