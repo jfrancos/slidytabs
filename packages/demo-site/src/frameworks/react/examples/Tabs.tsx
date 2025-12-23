2576e9128d009f61c61180090b1fb982b6859563
Justin Francos <justinfrancos@gmail.com>
2025-12-23 17:18:40 -0500

    update astro demo

--- packages/demo-site/src/frameworks/react/examples/Tabs.tsx
+++ packages/demo-site/src/frameworks/react/examples/Tabs.tsx
@@ -2,7 +2,7 @@ import { tabs } from "slidytabs";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/shadcn/react/tabs";
 
 export default () => (
-  <Tabs defaultValue="password" ref={tabs()} className="text-center">
+  <Tabs defaultValue="account" ref={tabs()} className="text-center">
     <TabsList>
       <TabsTrigger value="account">Account</TabsTrigger>
       <TabsTrigger value="password">Password</TabsTrigger>
