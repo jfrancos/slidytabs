7a5877b7ab642160ac18e046fd1b93f73355ed24
Justin Francos <justinfrancos@gmail.com>
2025-12-23 10:41:57 -0500

    centralize breakpoint

--- packages/demo-site/uno.config.ts
+++ packages/demo-site/uno.config.ts
@@ -9,14 +9,11 @@ import { presetShadcn } from "unocss-preset-shadcn";
 export default defineConfig({
   transformers: [transformerVariantGroup()],
   presets: [
-    presetWind4({
-      preflights: {
-        reset: true,
-      },
-    }),
+    presetWind4({ preflights: { reset: true } }),
     presetIcons,
     presetShadcn,
   ],
   safelist: ["highlighted-word"],
   shortcuts: { "highlighted-word": "bg-neutral-300 rounded-sm p-1 -m-1" },
+  theme: { breakpoint: { md: "38rem" } },
 });
