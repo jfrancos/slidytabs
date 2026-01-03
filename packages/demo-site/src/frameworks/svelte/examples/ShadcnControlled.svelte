698e343dffd572e346034183fc75df51aedbf1a8
Justin Francos <justinfrancos@gmail.com>
2026-01-03 18:08:57 -0500

    more svelte

---
+++ packages/demo-site/src/frameworks/svelte/examples/ShadcnControlled.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { tabs } from "slidytabs";
+  import { Tabs, List, Trigger, Content } from "@/shadcn/svelte/tabs";
+  let value = $state("correct");
+</script>
+
+<Tabs
+  {@attach tabs()}
+  bind:value={() => value, (next) => next !== "battery" && (value = next)}
+>
+  <List class="[&>:nth-child(3)]:!text-red">
+    <Trigger value="correct">Correct</Trigger>
+    <Trigger value="horse">Horse</Trigger>
+    <Trigger value="battery">Battery</Trigger>
+    <Trigger value="staple">Staple</Trigger>
+  </List>
+  <Content class="text-center" value="correct">Correct</Content>
+  <Content class="text-center" value="horse">Horse</Content>
+  <Content class="text-center" value="battery">Battery</Content>
+  <Content class="text-center" value="staple">Staple</Content>
+</Tabs>
---
+++ packages/demo-site/src/frameworks/svelte/examples/SlidytabsControlled.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { tabs } from "slidytabs";
+  import { Tabs, List, Trigger } from "@/shadcn/svelte/tabs";
+  const options = ["Correct", "Horse", "Battery", "Stapler"];
+
+  let index = $state(0);
+  const onValueChange = (next: number) => next !== 2 && (index = next);
+</script>
+
+<Tabs {@attach tabs({ value: index, onValueChange })} value="correct">
+  <List class="[&>:nth-child(3)]:!text-red">
+    {#each options as value}
+      <Trigger {value}>{value}</Trigger>
+    {/each}
+  </List>
+  {options[index]}
+</Tabs>
