6b4e8784aad76ea97b0e6807120972a9280d95db
Justin Francos <justinfrancos@gmail.com>
2026-01-04 14:26:24 -0500

    svelte style

--- packages/demo-site/src/frameworks/svelte/examples/Slider.svelte
+++ packages/demo-site/src/frameworks/svelte/examples/Slider.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { slider } from "slidytabs";
   import * as Tabs from "@/shadcn/svelte/tabs";
+  const triggerClasses =
+    "min-w-0 ring-inset rounded-lg h-full !shadow-none data-[state=active]:(bg-gray-300 rounded-none) data-[state=inactive]:text-neutral-500";
+
   let value = $state(5);
   const onValueChange = (newValue: number) => (value = newValue);
 </script>
@@ -11,9 +14,11 @@
       value={value.toString()}
       {@attach slider({ value, onValueChange })}
     >
-      <Tabs.List>
+      <Tabs.List class="p-0 overflow-hidden">
         {#each { length: 11 }, i}
-          <Tabs.Trigger class="min-w-0" value={i.toString()}>{i}</Tabs.Trigger>
+          <Tabs.Trigger class={triggerClasses} value={i.toString()}
+            >{i}</Tabs.Trigger
+          >
         {/each}
       </Tabs.List>
     </Tabs.Root>
