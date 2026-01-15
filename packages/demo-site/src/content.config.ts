5f5342ee7cd2a37449e8cdaf22d05d434bce87bc
Justin Francos <justinfrancos@gmail.com>
2026-01-15 00:13:14 -0500

    cleanup

--- packages/demo-site/src/content.config.ts
+++ packages/demo-site/src/content.config.ts
@@ -1,24 +1,19 @@
 import { defineCollection } from "astro:content";
 import * as readmeContent from "@base/README.md";
 import exampleContent from "./example-text.json";
-// import { remark } from "remark";
 import { remark } from "remark";
 import remarkToc from "remark-toc";
 
-console.log(readmeContent.rawContent());
-
-const readmeWithToc = await remark()
+const { value: readmeWithToc } = await remark()
   .use(remarkToc, { skip: "slidytabs|Quick start|React|Svelte|Vue|Bugs" })
   .process("## Contents\n" + readmeContent.rawContent());
-
-// console.log(toc);
 
 const readme = defineCollection({
   loader: {
     name: "readme",
     load: async ({ renderMarkdown, store }) => {
       store.clear();
-      const lines = readmeWithToc.value.split("\n");
+      const lines = (readmeWithToc as string).split("\n");
       let entry;
       for (const [index, line] of Object.entries(lines)) {
         if (line.startsWith("#") || Number(index) === lines.length - 1) {
