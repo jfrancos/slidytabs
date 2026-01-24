c06c36c92976f303840ae5a1e4c5a7a7e931896f
Justin Francos <justinfrancos@gmail.com>
2026-01-24 10:38:59 -0500

    set history

--- packages/demo-site/src/components/SelectFramework.svelte
+++ packages/demo-site/src/components/SelectFramework.svelte
@@ -28,14 +28,26 @@
       }
     }
   };
-  onMount(async () => {
+  onMount(() => {
     for (const framework of frameworks) {
       if (location.pathname === `/${framework}`) {
         frameworkAtom.set(framework);
-        updateState();
       }
     }
+    updateState();
   });
+  const setUrl = () => {
+    const entries = Object.entries(localStorage)
+      .filter(([key]) => key.startsWith("framework:"))
+      .map(([, v]) => v);
+    for (const framework of frameworks) {
+      if (entries.every((item) => item === framework)) {
+        history.replaceState(null, "", "/" + framework);
+        return;
+      }
+    }
+    history.replaceState(null, "", "/");
+  };
 </script>
 
 <Root
@@ -44,10 +56,11 @@
     onValueChange: (value) => {
       frameworkAtom.set(frameworks[value]);
       updateState();
+      setUrl();
     },
   })}
   class="items-end"
-  value="react"
+  value={$frameworkAtom}
 >
   <List class="*:px-2 *:gap-1 sm:*:px-4 sm:*:gap-2 *:h-7 rounded-md text-sm">
     {#each frameworks as framework}
